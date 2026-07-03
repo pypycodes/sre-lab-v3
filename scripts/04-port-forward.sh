15da77ed911dbb9632bc5b1d2d78ab58ea41f787
pypycodes <pypycodes@outlook.com>
2026-07-03 12:38:28 +0530

    added SRE Service RBAC etc

--- scripts/04-port-forward.sh
+++ scripts/04-port-forward.sh
@@ -8,7 +8,7 @@ echo "Grafana:    http://localhost:3000"
 echo "Prometheus: http://localhost:9090"
 echo "Alertmgr:   http://localhost:9093"
 
-kubectl port-forward svc/orders-service -n sre-lab 8080:8080 &
+# kubectl port-forward svc/orders-service -n sre-lab 8080:8080 &
 kubectl port-forward svc/monitoring-grafana -n monitoring 3000:80 &
 kubectl port-forward svc/monitoring-kube-prometheus-prometheus -n monitoring 9090:9090 &
 kubectl port-forward svc/monitoring-kube-prometheus-alertmanager -n monitoring 9093:9093 &
